7f647d80385504c5d4d2af197382c78565c5f199
ButchBet <k.salazar@utp.edu.co>
2021-10-26 19:39:16 -0500

    Aside almost finished, need to add rautes to each li

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -7,9 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
+  // Manupulate the asitde status 
+  icon:string | undefined;  // bars, x
+
+  // Save url, alt image  and name of user
+  imgUrl: string | undefined;
+
+  imgAlt: string | undefined;
+
+  personName: string | undefined;
+
+  // show or hidde the section, mane and footer
+  display: string | undefined;
+
+  // change width of aside
+  width: string | undefined;
+
+  // To move the formCheckBox
+  move: string | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.icon = "bars";
+
+    this.imgUrl = "personFront.jpg";
+
+    this.imgAlt = "Person profile";
+
+    this.personName = "Camila yulitza";
+
+    this.display = "hidden";
+
+    this.width = "minContainerWidht";
+  }
+
+  changeIcon() {
+    if(this.icon === "bars")  {
+      setTimeout(() => {
+        this.display = "";
+      },225);
+
+      this.width = "maxContainerWidht";
+
+      this.icon = "x";
+
+      this.move = "moveLeft1";
+    } else {
+      this.display = "hidden";
+
+      this.width = "minContainerWidht";
+
+      this.icon = "bars";
+
+      this.move = "";
+    }
   }
 
 }
