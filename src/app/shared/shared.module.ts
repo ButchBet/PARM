da5195f277eb974f4a79d4084b17b8a91f1825b8
Kevin Alejandro Salazar <53120163+ButchBet@users.noreply.github.com>
2021-10-27 09:52:02 -0500

    Update README.md

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedRoutingModule } from './shared-routing.module';
+
+
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistComponent } from './components/regist/regist.component';
-import { FormsModule } from '@angular/forms';
 import { ScheduleAppointmentComponent } from './components/schedule-appointment/schedule-appointment.component';
 import { SchedulePetitionComponent } from './components/schedule-petition/schedule-petition.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
@@ -26,7 +27,8 @@ import { AppointmentsComponent } from './components/appointments/appointments.co
   imports: [
     CommonModule,
     SharedRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
 
   exports: [
